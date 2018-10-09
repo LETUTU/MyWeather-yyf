141bcb6f3597078671e89ea0107c2d59e95169fe
yaoyangf <616369941@qq.com>
2018-10-09 14:24:46 +0800

    init project

---
+++ app/src/main/java/cn/edu/pku/yaoyangf/myweather/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.edu.pku.yaoyangf.myweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dell on 2018/10/9.
+ */
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
